554754686cb4e21784dbc3dec4149a0d8df7c13a
quentinsnow <quentin4996@gmail.com>
2016-11-25 10:30:54 +0100

    Create vaisseau.java

---
+++ vaisseau.java
@@ -0,0 +1,16 @@
+
+public class vaisseau extends entite
+{
+	private int integr ; 
+	private int res;
+	private propulsion prop; 
+	private equipement machin ;
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
