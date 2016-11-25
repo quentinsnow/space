3ba7d6c426093c967d20bb135207050aecf4e079
quentinsnow <quentin4996@gmail.com>
2016-11-25 10:29:43 +0100

    Create espece.java

---
+++ espece.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class espece 
+{
+	private int id;
+	private int txnat;
+	private int txprod;
+	private ArrayList<entite> truc;
+
+
+	public int getid()
+	{
+		return this.id;
+	}
+	public int getxnat()
+	{
+		return this.getxnat();
+	}
+	public int gettxnat()
+	{
+		return this.txnat;
+	}
+	public int gettxprod()
+	{
+		return this.txprod;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
