af1da3a1171d17498cc97d948508867da7e13e55
quentinsnow <quentin4996@gmail.com>
2016-11-25 10:30:05 +0100

    Create entite.java

---
+++ entite.java
@@ -0,0 +1,16 @@
+
+public abstract class entite 
+{
+	private int idE ;
+	private int posx;
+	private int posy; 
+	private boolean estoccupe; 
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
