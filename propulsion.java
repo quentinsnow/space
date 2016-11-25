c0d95f51ae6f1253a105d34cdeeeb607f1b917bb
quentinsnow <quentin4996@gmail.com>
2016-11-25 10:28:58 +0100

    Create propulsion.java

---
+++ propulsion.java
@@ -0,0 +1,15 @@
+
+public class propulsion 
+{
+	private int portee; 
+	private int reservoir;
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
