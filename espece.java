290f361580e10a90d863435ed8c5e4fcc86d6bd1
quentinsnow <quentin4996@gmail.com>
2016-12-02 10:44:17 +0100

    Update espece.java

--- espece.java
+++ espece.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Random;
 
 public class espece 
 {
@@ -6,7 +7,17 @@ public class espece
 	private int txnat;
 	private int txprod;
 	private ArrayList<entite> truc;
-
+	
+	public espece ()
+	{
+		Random rand = new Random();
+		this.id = Math.abs(rand.nextInt())%3;
+		this.txnat = Math.abs(rand.nextInt())%10+5;
+		this.txprod = Math.abs(rand.nextInt())%5+1;
+		// TODO ajouter affectation a une planete 
+		
+		
+	}
 
 	public int getid()
 	{
