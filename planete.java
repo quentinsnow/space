a484938c392fd5ba4ad7e9dcdc26925289b10964
quentinsnow <quentin4996@gmail.com>
2016-12-02 10:44:52 +0100

    Update planete.java

--- planete.java
+++ planete.java
@@ -1,9 +1,20 @@
+import java.util.Random;
+
 
 public class planete extends entite
 {
 	private int Taille; 
 	private int Popu;
 	private boolean Constr;
+	
+	public planete ()
+	{
+		super();
+		Random rand = new Random();
+		this.Taille = Math.abs(rand.nextInt())%(Constantes.PlaneteTailleMax-Constantes.PlaneteTailleMin+1) + Constantes.PlaneteTailleMin;
+		this.Popu = 0;
+		this.Constr = false;
+	}
 	
 	public int getTaille()
 	{
@@ -20,6 +31,9 @@ public class planete extends entite
 	public void augmpopu(espece alien)
 	{
 		Popu = Popu*(1+alien.gettxnat()/100);
+	}
+	public vaisseau construction()
+	{
 	}
 	/**
 	 * @param args
